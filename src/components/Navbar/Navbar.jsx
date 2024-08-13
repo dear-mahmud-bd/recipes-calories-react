fe6990a0686e4ed3204cf4b8841e98e9e872f296
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2024-08-13 17:30:21 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Hello, I&apos;m  Mahmud...</h1>
+        </div>
+    );
+};
+
+export default Navbar;
